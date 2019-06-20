e0f6a4c555069bcc8d9df3ead1079de3b5f8d9f4
Hema Venkata <hvenkata@live.com>
2019-06-20 17:18:29 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,15 +1,15 @@
 var playlist = {
   'My Bloody Valentine': "a song",
-  'Phil Ochs': "love on top",
-  'Slowdive': "April",
+  'beyonce': "love on top",
+  'Slowdive': "something",
+  'jonasBros': "sucker"
 };
 
 function updatePlaylist (playlist, artistName, songTitle) {
   return playlist[artistName] = songTitle
 }
 
-
 function removeFromPlaylist (playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist
 }
